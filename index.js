d81a9c171967a8836aaf9969f62d9e9b68a07e2e
Jason <jghan011@gmail.com>
2019-09-05 00:35:53 -0500

    added portfolio content

---
+++ index.js
@@ -0,0 +1,44 @@
+function handleClickBio() {
+    $(".bioLink").on("click", function (event) {
+        $(".bioPage").css("display", "block");
+        $(".projects").hide();
+        $(".homePage").hide();
+        $(".contactInfo").hide();
+
+
+    })
+
+}
+
+
+
+function handleClickPortfolio() {
+    $(".portfolio").on("click", function (event) {
+        $(".projects").css("display", "block");
+        $(".homePage").hide();
+        $(".contactInfo").hide();
+        $(".bioPage").hide();
+    })
+
+}
+
+
+function handleClickHome() {
+    $(".home").on("click", function (event) {
+        $(".homePage").show();
+        $(".projects").hide();
+        $(".bioPage").hide();
+        $(".contactInfo").hide();
+    })
+
+}
+
+
+function portfolio() {
+    handleClickBio();
+    handleClickPortfolio();
+    handleClickHome();
+
+
+}
+$(portfolio);
